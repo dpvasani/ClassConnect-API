b380f124d45cc07af467a30670e75a5e75fe2c35
dpvasani <vadanidarshan@gmail.com>
2024-08-16 10:11:48 +0530

    Some Changes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./src/db/dbconnect.js";
+const express = require('express');
+const connectDB = require('./db/dbConnection'); // Ensure the path is correct
+require('dotenv').config();
 
 dotenv.config(
     { 
@@ -10,6 +13,28 @@ dotenv.config(
 );
 
 connectDB();
+
+
+
+
+const teacherRoutes = require('./routes/teacherRoutes');
+const classroomRoutes = require('./routes/classroomRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/teachers', teacherRoutes);
+app.use('/api/classrooms', classroomRoutes);
+app.use('/api/students', studentRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
